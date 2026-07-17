4ab06b6d828e6a2f9ef4c5a873ae61a963f295c3
Renzo <renzomicchia@gmail.com>
2026-07-17 11:51:48 -0300

    Pipeline refactorizado, aún sin main

--- src/load_news.py
+++ src/load_news.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import sqlite3      #importamos sqlite3 desde Python (no es un paquete)
-from datetime import datetime, UTC
+from datetime import datetime, UTC, timedelta
 from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 import os
@@ -27,12 +27,17 @@ def obtener_noticias(query, desde, hasta):
     "apiKey": NEWS_API_KEY,    #apiKey seria nuestro "usuario" para pedirle cosas a la API
     "from": desde.isoformat(),
     "to": hasta.isoformat(),
+    #"from" : "2026-07-16T00:00:00",
+    #"to" : "2026-07-17T23:59:59",
     "sortBy": "publishedAt"
 }
     response = requests.get(url_api, params=params)
     
     if response.status_code == 200:
         articles = response.json()['articles']
+        #print(response.json()["totalResults"])
+        #print(response.json())
+        #print(response.url)
         return articles
         
     
@@ -42,126 +47,116 @@ def obtener_noticias(query, desde, hasta):
     
 zona_local = ZoneInfo("America/Argentina/Buenos_Aires")
 hasta = datetime.now(zona_local)
-desde = hasta.replace(
+"""desde = hasta.replace(
     hour = 0,
     minute = 0,
     second = 0,
     microsecond = 0
-)
-noticias = obtener_noticias("Argentina", desde, hasta)
+)"""
+desde = hasta - timedelta(hours=48)
+#desde = datetime(2026, 7, 16, 0, 0, 0)
+#hasta = datetime(2026, 7, 16, 23, 59, 59)
 
+noticias = obtener_noticias("World Cup", desde, hasta)
+#print(desde)
+#print(hasta)
 
-
-
-
-
+#Fin obtener_noticias()
 
 
 
 #Empieza guardar_noticias()
-fetched_at = datetime.now(zona_local).isoformat()
-
-conn = sqlite3.connect("database/db.db")   
-cursor = conn.cursor() 
-
-#total_articles = len(articles)
-#processed =
-inserted = 0 
-ignored = 0 
-urls_enviadas = []
-
-for article in noticias:
-    title = article['title']
-    source = article['source']['name']
-    description = article['description']
-    published_at = article['publishedAt']
-    url_article = article['url']
-    #print(json.dumps(article, indent=4))     
+def guardar_noticias(lista_noticias):
     
-    tupla_valores = (
-        title,
-        description,
-        source,
-        published_at,
-        url_article,
-        fetched_at
-    )  
-    
-                
-    cursor.execute("""        
+    fetched_at = datetime.now(zona_local).isoformat()
 
+    conn = sqlite3.connect("database/db.db")   
+    cursor = conn.cursor() 
 
-        INSERT OR IGNORE INTO raw_news (
+    urls_guardadas = []
+
+    for article in lista_noticias:
+        title = article['title']
+        source = article['source']['name']
+        description = article['description']
+        published_at = article['publishedAt']
+        url_article = article['url']
+        #print(json.dumps(article, indent=4))     
+        
+        tupla_valores = (
             title,
             description,
             source,
             published_at,
-            url,
+            url_article,
             fetched_at
-        )
-        VALUES (
-            ?,
-            ?,
-            ?,
-            ?,
-            ?,
-            ?
-        )
-        """, tupla_valores
-        )
-    
-    
-    cursor.rowcount                     #es un atributo del cursor que indica cuántas filas fueron afectadas por la última sentencia SQL que ejecutaste con ese cursor.
-    if cursor.rowcount == 1:
-        inserted += 1
-        urls_enviadas.append(url_article)
+        )  
         
-    else:
-        ignored += 1
+                    
+        cursor.execute("""        
+
+
+            INSERT OR IGNORE INTO raw_news (
+                title,
+                description,
+                source,
+                published_at,
+                url,
+                fetched_at
+            )
+            VALUES (
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?
+            )
+            """, tupla_valores
+            )
+        
+                   
+        if cursor.rowcount == 1:        #es un atributo del cursor que indica cuántas filas fueron afectadas por la última sentencia SQL que ejecutaste con ese cursor.
+            urls_guardadas.append(url_article)
+        
+
+    conn.commit()   
+    conn.close()            
     
-    #print(url_article)
-    #print(published_at)
-    #print(title)
-    
+    return urls_guardadas  
 
-
-
-conn.commit()   
-conn.close()              
+urls_guardadas = guardar_noticias(noticias)
 #Fin guardar_noticias()
-#print("Insertadas:", inserted)
-#print("Ignoradas:", ignored)
-#print("URLs enviadas:", urls_enviadas)
 
-#print(total_articles)
-#print(inserted)
-#print(ignored)
-#print(urls_enviadas)
-#print(len(urls_enviadas))        
-#print('\n'.join(urls_enviadas))
 
 #Empieza preparar_correo()
-if len(urls_enviadas) >= 1:
+def preparar_correo(lista_urls):
+    if len(lista_urls) >= 1:
     
-    msg = EmailMessage()
-    
-    msg['From'] = GMAIL_USER
-    msg['To'] = GMAIL_USER
-    msg['Subject'] = 'Noticias de hoy'
-    cuerpo_mail = '\n'.join(urls_enviadas)
-    msg.set_content(cuerpo_mail)
+        msg = EmailMessage()
+        
+        msg['From'] = GMAIL_USER
+        msg['To'] = GMAIL_USER
+        msg['Subject'] = 'Noticias de hoy'
+        cuerpo_mail = '\n'.join(lista_urls)
+        msg.set_content(cuerpo_mail)
+        return msg
+
+msg = preparar_correo(urls_guardadas)
 #Fin preparar_correo()
 
-    #Empieza enviar_correo()
+
+
+#Empieza enviar_correo()
+def enviar_correo(mensaje):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(GMAIL_USER, GMAIL_PASSWORD)
-    #print(GMAIL_USER)
-    #print(GMAIL_PASSWORD)
-    #print('Envío el correo')
-    server.send_message(msg)
+    server.send_message(mensaje)
     server.quit()
-    #Fin enviar_correo()
+    
+enviar_correo(msg)
+#Fin enviar_correo()
 
 
 
